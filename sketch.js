9c120f0f71e7a109b65888702a4f7f1f6ffcad89
Ariel Guerrero <ariel.guerrero@ucuenca.edu.ec>
2023-10-23 09:20:35 -0500

    un chage

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-pelotas = [];
+pelotas = []; //es una matriz que se utiliza para almacenar valores, los valores de cada pelota en la anumacion.
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -7,9 +7,11 @@ function setup() {
 
 function draw() {
   background(163, 99, 252);
+  // primero se establece el valor de la variable i (unica dentro del for) que es 0, luego indicamos al bucle for, que si i es menor que la longitud de la matriz pelotas este se seguira ejecutando, y al final le indicamos al bucle que despues de cada repeticion se incrementa el valor de i
+
   for (let i = 0; i < pelotas.length; i++) {
-    pelotas[i].update(windowHeight);
-    pelotas[i].display();
+    pelotas[i].update(windowHeight); //actualiza la posición y la velocidad de la pelota.
+    pelotas[i].display(); //e encarga de dibujar la pelota en el lienzo.
   }
 }
 
